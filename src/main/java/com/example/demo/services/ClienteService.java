cbae780a7b7ba5272116a9235655ec75daf1dda3
Joao Felipe <jfbnfelipe.jfbn@gmail.com>
2019-10-14 23:04:12 -0300

    cliente excluindo em cascata

--- src/main/java/com/example/demo/services/ClienteService.java
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -9,10 +9,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.Cidade;
 import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Endereco;
+import com.example.demo.domain.enums.TipoCliente;
 import com.example.demo.dto.ClienteDTO;
+import com.example.demo.dto.ClienteNewDTO;
+import com.example.demo.repositories.CidadeRepository;
 import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.repositories.EnderecoRepository;
 import com.example.demo.services.exceptions.DataIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +29,13 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		if (obj.empty() == null) {
@@ -30,15 +44,18 @@ public class ClienteService {
 		}
 		return obj.orElse(null);
 	}
-	
+
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
-		updateData(newObj,obj);
+		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
 
@@ -50,24 +67,58 @@ public class ClienteService {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
 		}
 	}
-	
-	public List<Cliente> findAll(){
+
+	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
 
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage,String orderBy, String direction){
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
-	public Cliente fromDTO(ClienteDTO  objDto) {
-		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCpnj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		
+		Optional<Cidade> cidOptional = cidadeRepository.findById(objDto.getCidadeId());
+		
+		Cidade cid = optionalFromCidade(cidOptional);
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+	}
+
+	private Cidade optionalFromCidade(Optional<Cidade> optionCidade) {
+		Cidade cidadeReturn = new Cidade();
+		cidadeReturn.setEstado(optionCidade.get().getEstado());
+		cidadeReturn.setNome(optionCidade.get().getNome());
+		cidadeReturn.setId(optionCidade.get().getId());
+		return cidadeReturn;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
-		
+
 	}
 
 }
